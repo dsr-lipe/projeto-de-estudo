55a895976d5e2050aad66e021afeeae14410d429
Felipe <felipedsrribeiro@gmail.com>
2025-10-20 19:14:04 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  // botao
+  const html = document.documentElement
+
+  // if (html.classList.contains('light')) {
+  //  html.classList.remove('light')
+  // else {
+  //  html.classList.add('light')
+  //}
+  html.classList.toggle("light")
+  // imagem
+  const img = document.querySelector("#p0 img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatarlight.png")
+    img.setAttribute("alt", "casal tirando uma foto no mar")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "foto de um casal durante seu casamento sentados numa poltrona"
+    )
+  }
+}
